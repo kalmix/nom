25b63fab310c8626a3cb80fcf647dc5baad21e9c
Kalmix <87293493+kalmix@users.noreply.github.com>
2025-10-10 15:13:41 -0400

    Add files via upload

---
+++ OOPNomina/DepartamentoEmpleado.cs
@@ -0,0 +1,17 @@
+﻿namespace OOPNomina
+{
+    public class DepartamentoEmpleado
+    {
+        private static int contadorId = 1;
+
+        public int Id { get; private set; } = 0;
+        public string Nombre { get; set; } = string.Empty;
+
+        public decimal BonificacionPorcentaje { get; set; } = 0;
+
+        public DepartamentoEmpleado()
+        {
+            Id = contadorId++;
+        }
+    }
+}
---
+++ OOPNomina/TipoEmpleado.cs
@@ -0,0 +1,16 @@
+﻿namespace OOPNomina
+{
+    public class TipoEmpleado
+    {
+        private static int contadorId = 1;
+
+        public int Id { get; private set; } = 0;
+        public string Nombre { get; set; } = string.Empty;
+        public decimal FactorSalario { get; set; } = 0;
+
+        public TipoEmpleado()
+        {
+            Id = contadorId++;
+        }
+    }
+}
